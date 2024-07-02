1ab47e3776f5f0f2051d92174ee5f6a4af60acf9
nurlumeryem <nurlumeryem1@gmail.com>
2024-07-02 20:16:03 +0300

    firebase_auth

--- lib/src/firebase_user_repo.dart
+++ lib/src/firebase_user_repo.dart
@@ -1,3 +1,4 @@
+import 'dart:developer'; // hata ayıklama
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tasks_app/src/model/user_model.dart';
@@ -10,21 +11,48 @@ class FirebaseUserRepo implements UserRepository {
   FirebaseUserRepo({
     FirebaseAuth? firebaseAuth,
   }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
-  @override
-  Stream<User?> get user => throw UnimplementedError();
 
   @override
-  Future<void> signIn(String email, String password) {
-    throw UnimplementedError();
+  Stream<User?> get user {
+    //Stream, kullanıcı her giriş yaptığında, çıkış yaptığında veya kimlik doğrulama durumu değiştiğinde tetiklenir.
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      return firebaseUser;
+    });
   }
 
   @override
-  Future<UserModel> signUp(UserModel user, String password) {
-    throw UnimplementedError();
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 
   @override
-  Future<void> setUserData(UserModel user) {
-    throw UnimplementedError();
+  Future<UserModel> signUp(UserModel userModel, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: userModel.email, password: password);
+      userModel = userModel.copyWith(userId: user.user!.uid);
+      return userModel;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> setUserData(UserModel userModel) async {
+    try {
+      await usersCollection
+          .doc(userModel.userId)
+          .set(userModel.toEntity().toDocument());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 }
